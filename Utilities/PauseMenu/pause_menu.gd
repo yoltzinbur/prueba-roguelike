ef0c65b32cdeb82497b1d4bce95b54bfac5c22a2
BimaSantiago <abimaelsantiago297@gmail.com>
2026-05-28 00:16:35 -0600

    Ojala no le mueva a lo que no debia

---
+++ Utilities/PauseMenu/pause_menu.gd
@@ -0,0 +1,35 @@
+extends Control
+
+func _ready():
+	$AnimationPlayer.play("RESET")
+
+func resume():
+	get_tree().paused=false
+	$AnimationPlayer.play_backwards("blur")
+	
+func pause():
+	get_tree().paused= true
+	$AnimationPlayer.play("blur")
+	
+func testEsc():
+	if Input.is_action_just_pressed("pausem") and get_tree().paused==false:
+		pause()
+	elif Input.is_action_just_pressed("pausem") and get_tree().paused==true:
+		resume()
+		
+
+func _on_reanudar_pressed() -> void:
+	resume() # Replace with function body.
+	
+
+func _on_reiniciar_pressed() -> void:
+	get_tree().paused=false
+	get_tree().reload_current_scene() # Replace with function body.
+	
+	
+
+func _on_salir_pressed() -> void:
+	get_tree().quit() # Replace with function body.
+	
+func _process(delta):
+	testEsc()
---
+++ Utilities/Tutorial/tutorial_ui.gd
@@ -0,0 +1,21 @@
+extends CanvasLayer
+
+@onready var panel = $Panel
+@onready var label = $Panel/Label
+
+func _ready():
+	print("TutorialUI cargada") 
+	TutorialManager.mostrar_mensaje.connect(_on_mostrar_mensaje)
+	TutorialManager.ocultar_mensaje.connect(_on_ocultar_mensaje)
+	panel.visible = false
+	
+	# Ahora SÍ pedimos el primer mensaje, porque ya estamos listos
+	TutorialManager.iniciar_tutorial()
+	print("iniciar_tutorial() llamado")
+
+func _on_mostrar_mensaje(texto: String):
+	label.text = texto
+	panel.visible = true
+
+func _on_ocultar_mensaje():
+	panel.visible = false
